de74a9b4718be37795fe95dbd7f989ab8a3f869f
Rodolfo <brsrkdev@gmail.com>
2020-01-27 16:15:20 -0600

    Fix linter errors

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
-const merge = require('webpack-merge');
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
